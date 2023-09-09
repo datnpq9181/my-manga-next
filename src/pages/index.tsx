f937fe548b8ab724eebccfe6c46424e1e34e2f2c
Dat <dat.ngo2994@gmail.com>
2023-09-10 00:26:35 +0700

    fix bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
-import React, { useState } from 'react';
-import Navbar from './navbar';
-import Manga from './manga';
+import Navbar from "./navbar";
+import Manga from "./manga";
 
 export default function Home() {
-  
-
   return (
     <div>
-      <div className="container mx-auto px-4 dark">
-        <Navbar/>
+      <div className="dark container mx-auto px-4">
+        <Navbar />
         <Manga />
       </div>
     </div>
--- src/pages/manga.tsx
+++ src/pages/manga.tsx
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 
 interface Manga {
-    id_book: number;
-    avatar: string;
-    name: string;
-    rss_chapter: string;
-  }
+  id_book: number;
+  avatar: string;
+  name: string;
+  rss_chapter: string;
+}
 
 const Manga = () => {
-    const [mangaList, setMangaList] = useState<Manga[]>([]);
+  const [mangaList, setMangaList] = useState<Manga[]>([]);
 
   useEffect(() => {
     void fetchMangaList();
@@ -20,7 +20,7 @@ const Manga = () => {
       const proxyUrl = "https://cors-anywhere.herokuapp.com/";
       const apiUrl = "https://cmangaaz.com/api/rss_new";
       const response = await fetch(proxyUrl + apiUrl);
-  
+
       if (response.ok) {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         const data: Manga[] = await response.json();
@@ -35,20 +35,20 @@ const Manga = () => {
 
   return (
     <div className="container mx-auto px-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {mangaList.map((manga) => (
           <div
-            className="manga-card overflow-hidden rounded-lg shadow-lg bg-white dark:bg-gray-800"
+            className="manga-card overflow-hidden rounded-lg bg-white shadow-lg dark:bg-gray-800"
             key={manga.id_book}
           >
             <div className="manga-image">
-              <Image
+              <img
                 src={manga.avatar}
                 alt={manga.name}
-                className="object-cover w-full h-56 sm:h-64 md:h-72"
+                className="h-56 w-full object-cover sm:h-64 md:h-72"
               />
             </div>
-            <h2 className="text-center mt-4 mb-2 text-lg font-semibold text-gray-800 dark:text-gray-300">
+            <h2 className="mb-2 mt-4 text-center text-lg font-semibold text-gray-800 dark:text-gray-300">
               <a
                 href={manga.rss_chapter}
                 target="_blank"
